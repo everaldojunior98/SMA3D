a4c497be112237f13426e5fef6e9abc649e6c9d3
everaldojunior98 <everaldojunior98@ucl.br>
2019-05-28 09:28:59 -0300

    [SMA3D] SMA3D - Unity Project - Added CommunicationManager: Connect, Read and Disconnect from COM port; - Added PortDropdown: Fill dropdown with all available COM ports;

---
+++ SMA3D - Unity Project/Assets/Scripts/CommunicationManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO.Ports;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class CommunicationManager : MonoBehaviour
+    {
+        public Dropdown PortsDropdown;
+
+        private bool _connected;
+        private SerialPort _port;
+
+        void Start()
+        {
+            _connected = false;
+        }
+
+        void Update()
+        {
+            if(!_connected || PortsDropdown == null)
+                return;
+
+            //Read data from COM port
+            try
+            {
+                Debug.Log(_port.ReadLine());
+            }
+            catch (TimeoutException) { }
+        }
+
+        //Close connection on exit from scene
+        void OnDestroy()
+        {
+            Disconnect();
+        }
+
+        //Create connection using COM port
+        public void Connect()
+        {
+            if(PortsDropdown == null)
+                return;
+
+            try
+            {
+                //Create port
+                _port = new SerialPort
+                {
+                    PortName = PortsDropdown.options[PortsDropdown.value].text,
+                    BaudRate = 9600,
+                    ReadTimeout = 2000,
+                    WriteTimeout = 2000
+                };
+
+                //Connect to port
+                _port.Open();
+                _connected = true;
+            }
+            catch
+            {
+                _connected = false;
+            }
+        }
+
+        //Disconnect if has valid connection
+        public void Disconnect()
+        {
+            if (_connected)
+                _port.Close();
+        }
+    }
+}
---
+++ SMA3D - Unity Project/Assets/Scripts/PortsDropdown.cs
@@ -0,0 +1,29 @@
+﻿using System.IO.Ports;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class PortsDropdown : MonoBehaviour
+    {
+        private Dropdown _dropdown;
+        void Start()
+        {
+            _dropdown = GetComponent<Dropdown>();
+        }
+
+        void Update()
+        {
+            //Return if dropdown is null
+            if(_dropdown == null)
+                return;
+
+            //Clear dropdown
+            _dropdown.options.Clear();
+
+            //Add all available ports on dropdown
+            foreach (var portName in SerialPort.GetPortNames())
+                _dropdown.options.Add(new Dropdown.OptionData(portName));
+        }
+    }
+}
